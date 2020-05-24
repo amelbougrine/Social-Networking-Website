c326b4ad5899485775bd0be8d41ca51ce2a855cd
Amelie121212 <amel.1989ab@gmail.com>
2020-05-24 23:50:55 +0100

    Left section

--- header.php
+++ header.php
@@ -8,42 +8,34 @@ if(isset($_SESSION['username'])){
     header("Location: register.php");
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-        <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
-        <link rel="stylesheet" type="text/css" href="assets/css/style-header.css">
-    </head>
-    <body>
-        <div class="header_bar">
-            <div class="nav-center">
-                <div class="dropdown">
-                    <?php echo "Hello";?>
-                    <span><img src="<?php echo $user['profile_pic']; ?>"></span>
-                    <div class="dropdown-content">
-                        <div class="dropdown-a">
-                            <h5><a href="<?php echo $userLoggedIn; ?>"><i style="color:#3875c5;" class="fas fa-user"></i>
-                                <?php echo $user ['username']?></a>
-                            </h5>
-                            <hr>
-                            <a href="requests.php"><i style="color:#3875c5;" class="fas fa-users"></i>&nbsp;Requests</a>
-                            <hr>
-                            <a href="account_settings.php"><i style="color:#3875c5;" class="fas fa-cog"></i>&nbsp;Settings</a><br><br>
-                            <a href="includes/handlers/logout.php"><i style="color:#3875c5;" class="fas fa-sign-out-alt"></i>&nbsp;Logout</a>
-                        </div>
-                    </div> 
-                    <?php echo $user['first_name']; ?><?php echo "!";?> 
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+<link rel="stylesheet" type="text/css" href="assets/css/style-header.css">
+<div class="header_bar">
+    <div class="nav-center">
+        <div class="dropdown">
+            <?php echo "Hello";?>
+            <span><img src="<?php echo $user['profile_pic']; ?>"></span>
+            <div class="dropdown-content">
+                <div class="dropdown-a">
+                    <h5><a href="<?php echo $userLoggedIn; ?>"><i style="color:#3875c5;" class="fas fa-user"></i>
+                        <?php echo $user ['username']?></a>
+                    </h5>
+                    <hr>
+                    <a href="requests.php"><i style="color:#3875c5;" class="fas fa-users"></i>&nbsp;Requests</a>
+                    <hr>
+                    <a href="account_settings.php"><i style="color:#3875c5;" class="fas fa-cog"></i>&nbsp;Settings</a><br><br>
+                    <a href="includes/handlers/logout.php"><i style="color:#3875c5;" class="fas fa-sign-out-alt"></i>&nbsp;Logout</a>
                 </div>
-            </div>
-            <nav>
-                <a href="index.php"><i style="color: #3875C5;" class="fas fa-home"></i>&nbsp;Home</a>&nbsp;&nbsp;
-                <a href=""><i style="color: #3875C5;" class="fas fa-envelope"></i>&nbsp;Messages</a>&nbsp;&nbsp;
-                <a href=""><i style="color: #3875C5;" class="fas fa-bell"></i>&nbsp;Notifications</a>
-          </nav>
+            </div> 
+            <?php echo $user['first_name']; ?><?php echo "!";?> 
         </div>
-    </body>
-</html>
+    </div>
+    <nav>
+        <a href="index.php"><i style="color: #3875C5;" class="fas fa-home"></i>&nbsp;Home</a>&nbsp;&nbsp;
+        <a href=""><i style="color: #3875C5;" class="fas fa-envelope"></i>&nbsp;Messages</a>&nbsp;&nbsp;
+        <a href=""><i style="color: #3875C5;" class="fas fa-bell"></i>&nbsp;Notifications</a>
+  </nav>
+</div>
 
--- index.php
+++ index.php
@@ -1,1 +1,33 @@
 <?php include 'header.php'; ?>
+<style type="text/css">
+    .wrapper{
+        width:75%;
+    }
+</style>
+
+<div class="index-wrapper">
+    <div class="info-box">
+        <div class="info-inner">
+            <div class="info-in-head">
+                <a href="<?php echo $userLoggedIn; ?>"><img src="<?php echo $user['cover_pic']; ?>"></a>
+            </div>
+            <div class="info-in-body">
+                <div class="in-b-box">
+                    <div class="in-b-img">
+                        <a href="<?php echo $userLoggedIn; ?>"><img src="<?php echo $user['profile_pic']; ?>"></a>
+                    </div>
+                </div>
+                <div class="info-body-name">
+                    <div class="in-b-name">
+                        <div>
+                            <a href="<?php echo $userLoggedIn; ?>"><?php echo $user['first_name'] . " " . $user['last_name']; ?></a>
+                        </div>
+                        <span><small>
+                            <a href="<?php echo $userLoggedIn; ?>"><?php echo "@" . $user['username'] ?></a>
+                        </small></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
