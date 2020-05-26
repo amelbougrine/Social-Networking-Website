159f9949b3a55b32764dc46d8107dfc4355d80c9
Amelie121212 <amel.1989ab@gmail.com>
2020-05-26 23:57:34 +0100

    body section 01

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     }
 </style>
 
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+<link rel="stylesheet" type="text/css" href="assets/css/style-header.css">
 <div class="index-wrapper">
     <div class="info-box">
         <div class="info-inner">
@@ -47,6 +50,37 @@
                                 <?php echo $user['num_likes']; ?>
                             </span>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="post-wrap">
+        <div class="post-inner">
+            <div clas="post-h-left">
+                <div class="post-h-img">
+                    <a href="<?php echo $userLoggedIn; ?>"><img src="<?php echo $user['profile_pic'] ?>"></a>
+                 </div>
+            </div>
+            <div class="post-body">
+                <form class="post_form" action="index.php" method="POST" enctype="multipart/form-data">
+                    <textarea class="status" name="post_text" id="post_text" placeholder="Type Something here!" rows="4" cols="50"></textarea>
+                    <div class="hash-box">
+                        <ul>
+                        </ul>
+                    </div>
+                    </div>
+                <div class="post-footer">
+                    <div class="p-fo-left">
+                        <ul>
+                            <input type="file" name="fileToUpload" id="fileToUpload"/>
+                            <label for="fileToUpload"><i style="color:#3875C5" class="fas fa-camera"></i></label>
+                            <span class="tweet-error"></span>
+                        </ul>
+                    </div>
+                    <div class="p-fo-right">
+                        <input type="submit" name="post" value="SHARE">
+                    </form>
                     </div>
                 </div>
             </div>
